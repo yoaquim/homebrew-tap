23e37af3f7ebc671b9e9de5ca84fafc6627906b1
Yoaquim Cintrón <yoaquim@outlook.com>
2026-04-19 13:36:30 -0400

    Add SHA256 for v1.0.0

--- Casks/covalent.rb
+++ Casks/covalent.rb
@@ -1,6 +1,6 @@
 cask "covalent" do
   version "1.0.0"
-  sha256 :no_check
+  sha256 "02869be5c31b2a3f4a56fa6b8fed310774ad7653c7ac8946f6f2034c6553f4dd"
 
   url "https://github.com/yoaquim/covalent/releases/download/v#{version}/Covalent_#{version}_aarch64.dmg"
   name "Covalent"
