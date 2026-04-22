a0d1fb07dc0c2a76a399bc0fd2d170de6f5959dc
Yoaquim Cintrón <yoaquim@outlook.com>
2026-04-22 09:06:40 -0400

    Update Covalent to 1.8.1

--- Casks/covalent.rb
+++ Casks/covalent.rb
@@ -1,6 +1,6 @@
 cask "covalent" do
-  version "1.8.0"
-  sha256 "c6f0945452196940815bb5274a533c2f3838d6395403bfa1090258c68b974df3"
+  version "1.8.1"
+  sha256 "011221d127a3b53075e97eca96e69e5da2dcf0cfc6e977ffcb2ac7b94c1db098"
 
   url "https://github.com/yoaquim/covalent/releases/download/v#{version}/Covalent_#{version}_aarch64.dmg"
   name "Covalent"
